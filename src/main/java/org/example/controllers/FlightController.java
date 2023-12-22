fa9e5061e760ad7bb0d82d29bebb7647e34c54ca
pgromiec <pgromiec@gmail.com>
2023-12-22 14:53:30 +0000

    choosing flights for period refactoring

--- src/main/java/org/example/controllers/FlightController.java
+++ src/main/java/org/example/controllers/FlightController.java
@@ -30,57 +30,26 @@ public class FlightController {
         return factory.getRequests();
     }
 
-    private void processSinglePeriod(Period initialPeriod, FlightRequestFactory factory) {
-        Period currentPeriod = initialPeriod;
-        System.out.println("Processing period: " + currentPeriod);
+    private void processSinglePeriod(Period period, FlightRequestFactory factory) {
+        System.out.println("Processing period: " + period);
 
-        List<Flight> flightsForCurrentPeriod = flightService.getFlightsForPeriod(currentPeriod); //todo refactor
+        List<Flight> flightsForCurrentPeriod = flightService.getFlightsForPeriod(period);
         if (flightsForCurrentPeriod.isEmpty()) {
             System.out.println("No flights for this period");
             return;
         }
 
-        Flight selectedFlight = periodController.chooseFlight(currentPeriod, factory);
+        Flight selectedFlight = periodController.chooseFlight(period, factory);
 
         if (selectedFlight == null) {
             System.out.println("No flight selected for this period.");
             return;
         }
 
-        List<Period> newCreatedPeriods = currentPeriod.splitPeriodAroundSelectedFlight(selectedFlight);
+        List<Period> newCreatedPeriods = period.splitPeriodAroundSelectedFlight(selectedFlight);
 
         processSinglePeriod(newCreatedPeriods.get(0), factory);
         processSinglePeriod(newCreatedPeriods.get(1),factory);
-    }
-
-    private boolean hasAvailableFlightsInThisPeriod(Period currentPeriod, List<Flight> flights) {
-        LocalDateTime startOfPeriod = currentPeriod.getStartTime();
-        LocalDateTime endOfPeriod = currentPeriod.getEndTime();
-
-        return flights.stream()
-                .anyMatch(flight -> !flight.getReportTime().isBefore(startOfPeriod) &&
-                        !flight.getClearTime().isAfter(endOfPeriod) &&
-                        flight.getReportTime().isBefore(flight.getClearTime()));
-    }
-
-    private List<Flight> getPossibleNextFlights(Flight selectedFlight, List<Flight> availableFlights) {
-        return availableFlights.stream()
-                .filter(nextFlight -> isSufficientBreakBetweenFlights(selectedFlight, nextFlight))
-                .collect(Collectors.toList());
-    }
-
-    private boolean isSufficientBreakBetweenFlights(Flight previousFlight, Flight nextFlight) {
-        Duration gap = Duration.between(previousFlight.getClearTime(), nextFlight.getReportTime());
-        Duration requiredGap = getRequiredBreak(previousFlight, nextFlight);
-        return gap.compareTo(requiredGap) >= 0;
-    }
-
-    private Duration getRequiredBreak(Flight previousFlight, Flight nextFlight) {
-        if (previousFlight.getFlightDuration().toHours() > 14 || nextFlight.getFlightDuration().toHours() > 14) {
-            return Duration.ofHours(18).plusMinutes(30);
-        } else {
-            return Duration.ofHours(12).plusMinutes(30);
-        }
     }
 
 
